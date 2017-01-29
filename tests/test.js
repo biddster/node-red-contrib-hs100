af7d18e6729d831482bcb5910876c1ad3ff45b3f
biddster <luke.biddell@gmail.com>
2017-01-29 21:37:25 +0000

    Initial commit.

---
+++ tests/test.js
@@ -0,0 +1,43 @@
+/**
+ The MIT License (MIT)
+
+ Copyright (c) 2017 @biddster
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
+
+"use strict";
+var assert = require('chai').assert;
+var mock = require('node-red-contrib-mock-node');
+var Hs100Api = require('fx-hs100-api');
+
+
+describe('hs100', function () {
+    describe('test', function () {
+        it('should turn a known socket on and off', function (done) {
+            var client = new Hs100Api.Client();
+            var plug = client.getPlug({host: '10.0.1.2'});
+            plug.setPowerState(true);
+            setTimeout(function () {
+                plug.setPowerState(false);
+                done();
+            }, 1000);
+        });
+    });
+});
